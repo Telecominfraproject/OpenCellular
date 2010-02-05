3a878528dad1500e1a1c251b59d5206edd92b02a
Gaurav Shah <gauravsh@google.com>
2010-02-04 18:58:48 -0800

    Fix copyright header on padding.h.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/570038

--- include/padding.h
+++ include/padding.h
@@ -1,3 +1,8 @@
+/* Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 #ifndef VBOOT_REFERENCE_PADDING_H_
 #define VBOOT_REFERENCE_PADDING_H_
 
