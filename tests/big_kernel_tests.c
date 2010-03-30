d0f3298bb110f45bb850740ee51edb4f1550cd20
Gaurav Shah <gauravsh@chromium.org>
2010-03-30 00:37:38 -0700

    Explicitly use the ULL suffix to specify a large constant.
    This hopefully fixes the ARM build error.
    
    TBR = wad@chromium.org
    
    Review URL: http://codereview.chromium.org/1591001

--- tests/big_kernel_tests.c
+++ tests/big_kernel_tests.c
@@ -16,7 +16,7 @@
 #include "utility.h"
 
 /* Choose a kernel size greater than the range of 32-bits unsigned. */
-#define BIG_KERNEL_SIZE ((uint64_t) 0x100000000)
+#define BIG_KERNEL_SIZE ((uint64_t) 0x100000000ULL)
 
 #define FIRMWARE_KEY_BASE_NAME "testkeys/key_rsa2048"
 #define KERNEL_KEY_BASE_NAME "testkeys/key_rsa1024"
