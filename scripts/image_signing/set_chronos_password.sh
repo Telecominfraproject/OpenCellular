5584d5bc5a4ee4dea141cf2e36eda090a5c79248
Darin Petkov <petkov@chromium.org>
2010-08-11 09:55:08 -0700

    Change chronos password rather than root.
    Also, rename the script to reflect its specific purpose.
    
    BUG=5580
    TEST=ran on an image, installed and tested with new password
    
    Review URL: http://codereview.chromium.org/3175003

--- scripts/image_signing/set_chronos_password.sh
+++ scripts/image_signing/set_chronos_password.sh
@@ -4,19 +4,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Customize a Chrome OS release image. The cgpt utility must be on the
-# sudo path.
-#
-# The following changes are applied:
-# - Set the root password.
+# Customizes a Chrome OS release image by setting the chronos user password.
 
-# Usage: ./customize_image.sh <image.bin> <root_password>
+# Usage: ./set_chronos_password.sh <image.bin> <chronos_password>
 
 # Load common constants and variables.
 . "$(dirname "$0")/common.sh"
 
 readonly ROOTFS_DIR=$(mktemp -d)
-readonly GPT=cgpt
 
 cleanup() {
   set +e
@@ -30,31 +25,32 @@ failure() {
   exit 1
 }
 
-change_root_password() {
+change_chronos_password() {
   local password=$1
-  echo "Changing root password to '$password'..."
+  echo "Changing chronos password to '$password'..."
   local crypted_password="$(echo $password | openssl passwd -1 -stdin)"
   local temp_shadow="$ROOTFS_DIR/etc/tempshadow"
-  echo "root:$crypted_password:14500:0:::::" \
+  echo "chronos:$crypted_password:14500:0:99999::::" \
     | sudo tee "$temp_shadow" > /dev/null
-  grep -Ev ^root: "$ROOTFS_DIR/etc/shadow" \
+  grep -Ev ^chronos: "$ROOTFS_DIR/etc/shadow" \
     | sudo tee -a "$temp_shadow" > /dev/null
   sudo mv -f "$temp_shadow" "$ROOTFS_DIR/etc/shadow"
 }
 
 main() {
   local image=$1
-  local root_password=$2
+  local chronos_password=$2
   if [ $# -ne 2 ]; then
-    echo "Usage: $0 <image.bin> <root_password>"
+    echo "Usage: $0 <image.bin> <chronos_password>"
     exit 1
   fi
 
   set -e
   trap failure EXIT
   mount_image_partition "$image" 3 $ROOTFS_DIR
-  change_root_password "$root_password"
+  change_chronos_password "$chronos_password"
   cleanup
+  touch "$image"  # Updates the image modification time.
   echo "Done."
   trap - EXIT
 }
