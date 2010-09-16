474f055c1b86ac96f378394e12c68fcfaba17521
Gaurav Shah <gauravsh@chromium.org>
2010-09-16 14:34:54 -0700

    Add script to remove the test label from lsb-release descriptions of an image.
    Change-Id: I7978568414c935eb625539f3ca5af5002e08931f
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3429011

---
+++ scripts/image_signing/remove_test_label.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Remove the test label from lsb-release to prepare an image for
+# signing using the official keys.
+
+# Load common constants and variables.
+. "$(dirname "$0")/common.sh"
+
+set -e
+image=$1
+
+rootfs=$(mktemp -d)
+mount_image_partition ${image} 3 ${rootfs}
+trap "umount -d ${rootfs}; rm -rf ${rootfs}" EXIT
+sed -i 's/test//' "${rootfs}/etc/lsb-release"
