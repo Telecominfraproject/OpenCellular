387b91f908a115d27efa2c572b98182f9b741f00
Jim Hebert <jimhebert@chromium.org>
2011-01-27 17:46:24 -0800

    Script to check for blacklisted files as described in chrome-os-partner:1991
    Change-Id: Iad5578031e4d62435a27c92651b1c91075987078
    
    BUG=chrome-os-partner:1991
    TEST=Ran manually against current mario images, 'base' img passes, 'test' fails, as expected
    
    Review URL: http://codereview.chromium.org/6264023

---
+++ scripts/image_signing/ensure_no_nonrelease_files.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Abort on error.
+set -e
+
+# Load common constants and variables.
+. "$(dirname "$0")/common.sh"
+
+usage() {
+    echo "Usage $PROG image [config]"
+}
+
+main() {
+    # We want to catch all the discrepancies, not just the first one.
+    # So, any time we find one, we set testfail=1 and continue.
+    # When finished we will use testfail to determine our exit value.
+    local testfail=0
+
+    if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+        usage
+        exit 1
+    fi
+
+    local image="$1"
+
+    # Default config location: same name/directory as this script,
+    # with a .config file extension, ie ensure_no_nonrelease_files.config.
+    local configfile="$(dirname "$0")/${0/%.sh/.config}"
+    # Or, maybe a config was provided on the command line.
+    if [ $# -eq 2 ]; then
+        configfile="$2"
+    fi
+    # Either way, load test-expectations data from config.
+    . "$configfile"
+
+    local rootfs=$(make_temp_dir)
+    mount_image_partition_ro "$image" 3 "$rootfs"
+
+    for file in ${RELEASE_FILE_BLACKLIST[@]}; do
+        if [ -e "$rootfs/$file" ]; then
+            echo "FAIL: $file exists in this image!"
+            ls -al "$rootfs/$file"
+            testfail=1
+        fi
+    done
+
+    exit $testfail
+}
+main $@
