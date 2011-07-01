3106d7ce3e0b40f9170a115ab3dcad5315a92d49
Bill Richardson <wfrichar@chromium.org>
2011-07-01 13:10:37 -0700

    Refactor bitmap directories and master images for ARM work.
    This change moves the old bitmaps (Mario, Alex, ZGB) and their supporting
    scripts into a subdirectory, and creates a new set of images at 1366x768, in
    PNG format.
    
    This is preparation for providing a complete set of localized BIOS screens
    to use as the master for all new platforms.
    
    The plan is that these master images will be scaled, cropped, and converted
    into the correct formats for each target platform, and those binary
    bmpblocks saved in their own package. Only if a translation changes should
    we need to regenerate the bmpblocks.
    
    These new images do NOT (yet) include locales that cannot be rendered
    correctly by ImageMagick, and not all of them have been fully vetted by the
    localization team.
    
    BUG=chromium-os:13037
    TEST=none
    
    Change-Id: Ic25832aad3c6cc36879db204c2579395014af311
    Reviewed-on: http://gerrit.chromium.org/gerrit/5508
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Bill Richardson <wfrichar@chromium.org>
    Reviewed-by: Bill Richardson <wfrichar@chromium.org>

---
+++ scripts/newbitmaps/old_releases/mario/makelines.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+use strict;
+our $opt_u = 'http://www.chromium.org';
+our $opt_m = 'Unsupported Prototype 0000';
+our $opt_d = '.';
+
+use File::Basename;
+my $progdir = dirname($0);
+my $prog = basename($0);
+
+use Getopt::Std;
+my $usage = "
+Usage:  $prog
+
+";
+getopts('u:m:d:') or die $usage;
+
+my @old = glob("$opt_d/linetxt_*");
+unlink(@old) if @old;
+
+$/ = undef;
+$_ = <>;
+s/\s+$//gs;
+
+my $count = 1;
+foreach (split(/\n/, $_))
+{
+    s/^\s+//;
+    s/\s+$//;
+    s/\$URL/$opt_u/g;
+    s/\$MODEL/$opt_m/g;
+    $_ = ' ' unless $_;
+    my $big = s/^\$BIG:\s*//;
+    my $filename = sprintf('%s/linetxt_%02d.%s', $opt_d, $count++,
+                           $big ? 'TXT' : 'txt');
+#    print "$filename: ($_)\n"; next;
+    open(OUT, ">$filename") || die "$0 can't write $filename: $!\n";
+    print OUT "$_";
+    close(OUT);
+}
