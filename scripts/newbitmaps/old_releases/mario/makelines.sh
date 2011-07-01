d52b3dfde2c483f3604ff5fcb9ce3eabe771dc7a
Bill Richardson <wfrichar@chromium.org>
2011-07-01 13:10:37 -0700

    Move bitmaps for shipping products into a separate directory.
    This is the first step in reorganizing and updating the bitmaps for all
    locales.
    
    BUG=chromium-os:13037
    TEST=none
    
    Change-Id: I95ad4c20fde7cb5eefeb11ef50b6f0ead886bc3c
    Reviewed-on: http://gerrit.chromium.org/gerrit/4424
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Reviewed-by: Bill Richardson <wfrichar@chromium.org>
    Tested-by: Bill Richardson <wfrichar@chromium.org>

--- scripts/newbitmaps/old_releases/mario/makelines.sh
+++ scripts/newbitmaps/old_releases/mario/makelines.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/perl
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 use strict;
 our $opt_u = 'http://www.chromium.org';
