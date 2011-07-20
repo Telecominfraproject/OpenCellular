4312770fb197c237e560d18c9247425a1df23764
Randall Spangler <rspangler@chromium.org>
2011-07-20 11:02:38 -0700

    Change description of dev_boot_usb
    Now indicates it covers SD as well (it did already, but now it's clearer).
    
    BUG=chromium-os:17907
    TEST=run `crossystem`; look at new descriptions
    
    Change-Id: I4e4d8502b0dc5a29eb403039535b7512941ab4fa
    Reviewed-on: http://gerrit.chromium.org/gerrit/4408
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Tested-by: Randall Spangler <rspangler@chromium.org>

--- utility/crossystem_main.c
+++ utility/crossystem_main.c
@@ -38,7 +38,7 @@ const Param sys_param_list[] = {
   {"cros_debug", 0, "OS should allow debug features"},
   {"dbg_reset", CAN_WRITE, "Debug reset mode request (writable)"},
   {"dev_boot_usb", CAN_WRITE,
-   "Enable developer mode boot from USB (writable)"},
+   "Enable developer mode boot from USB/SD (writable)"},
   {"devsw_boot", 0, "Developer switch position at boot"},
   {"devsw_cur",  0, "Developer switch current position"},
   {"ecfw_act", IS_STRING, "Active EC firmware"},
