40bc7b7802e89b009c3dbcd0a63cbd44d411659c
Kris Rambish <krisr@chromium.org>
2012-09-19 18:16:16 -0700

    Do not create the kernel data keys.
    BUG=None
    TEST=Ran it
    BRANCH=None
    
    Change-Id: Ib494c64d81c4ee80991a01b2172c7c47b60d5658
    Reviewed-on: https://gerrit.chromium.org/gerrit/33659
    Tested-by: Kris Rambish <krisr@chromium.org>
    Reviewed-by: Gaurav Shah <gauravsh@chromium.org>

--- scripts/keygeneration/increment_kernel_subkey.sh
+++ scripts/keygeneration/increment_kernel_subkey.sh
@@ -28,15 +28,13 @@ main() {
   new_firm_ver=$(increment_version "${KEY_DIR}" "firmware_version")
 
   backup_existing_kernel_subkeys ${CURR_FIRM_VER} ${CURR_KERNKEY_VER}
-  backup_existing_kernel_data_keys ${CURR_FIRM_VER} ${CURR_KERNKEY_VER}
 
   cat <<EOF
-Generating new kernel subkey, data keys and new kernel keyblock.
+Generating new kernel subkey and new kernel keyblock.
 
 New Firmware version (due to kernel subkey change): ${new_firm_ver}.
 EOF
   make_pair kernel_subkey ${KERNEL_SUBKEY_ALGOID} ${new_firm_ver}
-  make_pair kernel_data_key ${KERNEL_DATAKEY_ALGOID} ${CURRENT_KDATAKEY_VERSION}
   make_keyblock kernel $KERNEL_KEYBLOCK_MODE kernel_data_key kernel_subkey
 
   write_updated_version_file ${CURR_FIRMKEY_VER} ${new_firm_ver} \
