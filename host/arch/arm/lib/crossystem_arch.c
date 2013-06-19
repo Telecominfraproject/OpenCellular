114d54a9e26aad6bfa4564152a3f1daed2e75a9b
Vadim Bendebury <vbendeb@chromium.org>
2013-06-19 16:44:27 -0700

    Add 5420 to the set of recognizable platforms
    It is used on peach_pit.
    
    BRANCH=none
    BUG=none
    TEST=manual
       . on peach-pit:
    
      localhost ~ # echo $(crossystem arch)
      arm
      localhost ~ #
    
    Change-Id: Ia9a4ea2291d6b672fca1c9e1305961eedc4f60cf
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/59339
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>

--- host/arch/arm/lib/crossystem_arch.c
+++ host/arch/arm/lib/crossystem_arch.c
@@ -59,6 +59,7 @@ const PlatformFamily platform_family_array[] = {
   {"ti,omap3", "OMAP3"},
   {"samsung,exynos4210", "EXYNOS4"},
   {"samsung,exynos5250", "EXYNOS5"},
+  {"samsung,exynos5420", "EXYNOS5"},
   /* Terminate with NULL entry */
   {NULL, NULL}
 };
