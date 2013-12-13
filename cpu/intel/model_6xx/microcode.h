ed5df7bd12338bb20e70dccb449b1d11b555c7b5
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2013-12-12 23:22:15 -0600

    cpu/intel: Add microcode files for supported CPUs
    Microcode files will need to be added to this repository before they
    can be removed from the main coreboot repo. Add them in anticipation
    of this change. The script was updated to pull the latest microcode.
    
    These files were extracted using the update-microcodes.sh script, and
    may not necessarily match the updates currently present in the main
    repository.
    
    Change-Id: I30d41ff31b1ebb6aaeb773c2c663d7176d27060d
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

---
+++ cpu/intel/model_6xx/microcode.h
@@ -0,0 +1,11 @@
+
+#include "microcode-MU16600a.h"
+#include "microcode-MU166503.h"
+#include "microcode-MU166a0b.h"
+#include "microcode-MU166a0c.h"
+#include "microcode-MU166a0d.h"
+#include "microcode-MU166d05.h"
+#include "microcode-MU166d06.h"
+#include "microcode-MU166d07.h"
+#include "microcode-mu26a003.h"
+#include "microcode-mu26a101.h"
---
+++ cpu/intel/model_f0x/microcode.h
@@ -0,0 +1,6 @@
+
+#include "microcode-2f0708.h"
+#include "microcode-m01f0712.h"
+#include "microcode-m01f0a13.h"
+#include "microcode-m02f0a15.h"
+#include "microcode-m04f0a14.h"
