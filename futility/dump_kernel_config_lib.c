785343089c8fa38eacf9a71a33618125bb4174b9
Doug Evans <dje@google.com>
2015-11-19 15:05:45 -0800

    Include <fcntl.h> instead of <sys/fcntl.h>.
    Musl complains about using sys/fcntl.h, and with -Werror that
    gives a compilation failure. Every other file includes <fcntl.h>
    so just use that.
    
    Change-Id: Ibc743bbe335e9e9b91da7e4eacc01cc80cd8b35a
    Reviewed-on: https://chromium-review.googlesource.com/313419
    Commit-Ready: Doug Evans <dje@google.com>
    Tested-by: Doug Evans <dje@google.com>
    Reviewed-by: Nam Nguyen <namnguyen@chromium.org>

--- futility/dump_kernel_config_lib.c
+++ futility/dump_kernel_config_lib.c
@@ -5,10 +5,10 @@
  * Exports the kernel commandline from a given partition/image.
  */
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/mman.h>
-#include <sys/fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
