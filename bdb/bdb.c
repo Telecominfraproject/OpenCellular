b37e6672ebb4be7f2a3ec7d55a3e322f7afe1900
Randall Spangler <rspangler@chromium.org>
2015-12-10 15:03:22 -0800

    Add BDB library
    This is a standalone library for verifying the BDB structures in the
    common boot flow document, and a bdb_create utility to create test BDB
    structures.  Eventually, creating these structures will be rolled into
    futility.
    
    BUG=chrome-os-partner:48448
    BRANCH=none
    TEST=cd bdb && make runtests
    
    Change-Id: Ic57c26ca84137205da3b6c7d532f5324c93b4285
    Signed-off-by: Randall Spangler <rspangler@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/317275
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

---
+++ bdb/bdb.c
@@ -0,0 +1,398 @@
+/* Copyright (c) 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Boot descriptor block firmware functions
+ */
+
+#include <string.h>
+#include "bdb.h"
+
+/*****************************************************************************/
+
+/**
+ * Check if string contains a null terminator.
+ *
+ * Bytes after the null terminator do not need to be null.
+ *
+ * @param s		String to check
+ * @param size		Size of string buffer in characters
+ * @return 1 if string has a null terminator, 0 if not
+ */
+int string_has_null(const char *s, size_t size)
+{
+	for (; size; size--) {
+		if (*s++ == 0)
+			return 1;
+	}
+	return 0;
+}
+
+int bdb_check_header(const struct bdb_header *p, size_t size)
+{
+	if (size < sizeof(*p) || size < p->struct_size)
+		return BDB_ERROR_BUF_SIZE;
+
+	if (p->struct_magic != BDB_HEADER_MAGIC)
+		return BDB_ERROR_STRUCT_MAGIC;
+
+	if (p->struct_major_version != BDB_HEADER_VERSION_MAJOR)
+		return BDB_ERROR_STRUCT_VERSION;
+
+	/* Note that minor version doesn't matter yet */
+
+	if (p->struct_size < sizeof(*p))
+		return BDB_ERROR_STRUCT_SIZE;
+
+	if (p->oem_area_0_size & 3)
+		return BDB_ERROR_OEM_AREA_SIZE;  /* Not 32-bit aligned */
+
+	/*
+	 * Make sure the BDB is at least big enough for us.  At this point, all
+	 * the caller may have loaded is this header We'll check if there's
+	 * space for everything else after we load it.
+	 */
+	if (p->bdb_size < sizeof(*p))
+		return BDB_ERROR_BDB_SIZE;
+
+	/*
+	 * The rest of the fields don't matter yet; we'll check them when we
+	 * check the BDB itself.
+	 */
+	return BDB_SUCCESS;
+}
+
+int bdb_check_key(const struct bdb_key *p, size_t size)
+{
+	size_t expect_key_size = 0;
+
+	if (size < sizeof(*p) || size < p->struct_size)
+		return BDB_ERROR_BUF_SIZE;
+
+	if (p->struct_magic != BDB_KEY_MAGIC)
+		return BDB_ERROR_STRUCT_MAGIC;
+
+	if (p->struct_major_version != BDB_KEY_VERSION_MAJOR)
+		return BDB_ERROR_STRUCT_VERSION;
+
+	/* Note that minor version doesn't matter yet */
+
+	if (!string_has_null(p->description, sizeof(p->description)))
+		return BDB_ERROR_DESCRIPTION;
+
+	/* We currently only support SHA-256 */
+	if (p->hash_alg != BDB_HASH_ALG_SHA256)
+		return BDB_ERROR_HASH_ALG;
+
+	/* Make sure signature algorithm and size are correct */
+	switch (p->sig_alg) {
+	case BDB_SIG_ALG_RSA4096:
+		expect_key_size = BDB_RSA4096_KEY_DATA_SIZE;
+		break;
+	case BDB_SIG_ALG_ECSDSA521:
+		expect_key_size = BDB_ECDSA521_KEY_DATA_SIZE;
+		break;
+	case BDB_SIG_ALG_RSA3072B:
+		expect_key_size = BDB_RSA3072B_KEY_DATA_SIZE;
+		break;
+	default:
+		return BDB_ERROR_SIG_ALG;
+	}
+
+	if (p->struct_size < sizeof(*p) + expect_key_size)
+		return BDB_ERROR_STRUCT_SIZE;
+
+	return BDB_SUCCESS;
+}
+
+int bdb_check_sig(const struct bdb_sig *p, size_t size)
+{
+	size_t expect_sig_size = 0;
+
+	if (size < sizeof(*p) || size < p->struct_size)
+		return BDB_ERROR_BUF_SIZE;
+
+	if (p->struct_magic != BDB_SIG_MAGIC)
+		return BDB_ERROR_STRUCT_MAGIC;
+
+	if (p->struct_major_version != BDB_SIG_VERSION_MAJOR)
+		return BDB_ERROR_STRUCT_VERSION;
+
+	/* Note that minor version doesn't matter yet */
+
+	if (!string_has_null(p->description, sizeof(p->description)))
+		return BDB_ERROR_DESCRIPTION;
+
+	/* We currently only support SHA-256 */
+	if (p->hash_alg != BDB_HASH_ALG_SHA256)
+		return BDB_ERROR_HASH_ALG;
+
+	/* Make sure signature algorithm and size are correct */
+	switch (p->sig_alg) {
+	case BDB_SIG_ALG_RSA4096:
+		expect_sig_size = BDB_RSA4096_SIG_SIZE;
+		break;
+	case BDB_SIG_ALG_ECSDSA521:
+		expect_sig_size = BDB_ECDSA521_SIG_SIZE;
+		break;
+	case BDB_SIG_ALG_RSA3072B:
+		expect_sig_size = BDB_RSA3072B_SIG_SIZE;
+		break;
+	default:
+		return BDB_ERROR_SIG_ALG;
+	}
+
+	if (p->struct_size < sizeof(*p) + expect_sig_size)
+		return BDB_ERROR_STRUCT_SIZE;
+
+	return BDB_SUCCESS;
+}
+
+int bdb_check_data(const struct bdb_data *p, size_t size)
+{
+	size_t need_size;
+
+	if (size < sizeof(*p) || size < p->signed_size)
+		return BDB_ERROR_BUF_SIZE;
+
+	if (p->struct_magic != BDB_DATA_MAGIC)
+		return BDB_ERROR_STRUCT_MAGIC;
+
+	if (p->struct_major_version != BDB_DATA_VERSION_MAJOR)
+		return BDB_ERROR_STRUCT_VERSION;
+
+	/* Note that minor version doesn't matter yet */
+
+	if (!string_has_null(p->description, sizeof(p->description)))
+		return BDB_ERROR_DESCRIPTION;
+
+	if (p->struct_size < sizeof(*p))
+		return BDB_ERROR_STRUCT_SIZE;
+
+	if (p->hash_entry_size < sizeof(struct bdb_hash))
+		return BDB_ERROR_HASH_ENTRY_SIZE;
+
+	/* Calculate expected size */
+	need_size = p->struct_size + p->num_hashes * p->hash_entry_size;
+
+	/* Make sure OEM area size doesn't cause wraparound */
+	if (need_size + p->oem_area_1_size < need_size)
+		return BDB_ERROR_OEM_AREA_SIZE;
+	if (p->oem_area_1_size & 3)
+		return BDB_ERROR_OEM_AREA_SIZE;  /* Not 32-bit aligned */
+	need_size += p->oem_area_1_size;
+
+	if (p->signed_size < need_size)
+		return BDB_ERROR_SIGNED_SIZE;
+
+	return BDB_SUCCESS;
+}
+
+/*****************************************************************************/
+
+const struct bdb_header *bdb_get_header(const void *buf)
+{
+	return buf;
+}
+
+const struct bdb_key *bdb_get_bdbkey(const void *buf)
+{
+	const struct bdb_header *h = bdb_get_header(buf);
+	const uint8_t *b8 = buf;
+
+	/* BDB key follows header */
+	return (const struct bdb_key *)(b8 + h->struct_size);
+}
+
+const void *bdb_get_oem_area_0(const void *buf)
+{
+	const struct bdb_key *k = bdb_get_bdbkey(buf);
+	const uint8_t *b8 = (const uint8_t *)k;
+
+	/* OEM area 0 follows BDB key */
+	return b8 + k->struct_size;
+}
+
+const struct bdb_key *bdb_get_subkey(const void *buf)
+{
+	const struct bdb_header *h = bdb_get_header(buf);
+	const uint8_t *b8 = bdb_get_oem_area_0(buf);
+
+	/* Subkey follows OEM area 0 */
+	return (const struct bdb_key *)(b8 + h->oem_area_0_size);
+}
+
+const struct bdb_sig *bdb_get_header_sig(const void *buf)
+{
+	const struct bdb_header *h = bdb_get_header(buf);
+	const uint8_t *b8 = bdb_get_oem_area_0(buf);
+
+	/* Header signature starts after signed data */
+	return (const struct bdb_sig *)(b8 + h->signed_size);
+}
+
+const struct bdb_data *bdb_get_data(const void *buf)
+{
+	const struct bdb_sig *s = bdb_get_header_sig(buf);
+	const uint8_t *b8 = (const uint8_t *)s;
+
+	/* Data follows header signature */
+	return (const struct bdb_data *)(b8 + s->struct_size);
+}
+
+const void *bdb_get_oem_area_1(const void *buf)
+{
+	const struct bdb_data *p = bdb_get_data(buf);
+	const uint8_t *b8 = (const uint8_t *)p;
+
+	/* OEM area 1 follows BDB data */
+	return b8 + p->struct_size;
+}
+
+const struct bdb_hash *bdb_get_hash(const void *buf, enum bdb_data_type type)
+{
+	const struct bdb_data *data = bdb_get_data(buf);
+	const uint8_t *b8 = bdb_get_oem_area_1(buf);
+	int i;
+
+	/* Hashes follow OEM area 0 */
+	b8 += data->oem_area_1_size;
+
+	/* Search for a matching hash */
+	for (i = 0; i < data->num_hashes; i++, b8 += data->hash_entry_size) {
+		const struct bdb_hash *h = (const struct bdb_hash *)b8;
+
+		if (h->type == type)
+			return h;
+	}
+
+	return NULL;
+}
+
+const struct bdb_sig *bdb_get_data_sig(const void *buf)
+{
+	const struct bdb_data *data = bdb_get_data(buf);
+	const uint8_t *b8 = (const uint8_t *)data;
+
+	/* Data signature starts after signed data */
+	return (const struct bdb_sig *)(b8 + data->signed_size);
+}
+
+/*****************************************************************************/
+
+int bdb_verify_sig(const struct bdb_key *key,
+		   const struct bdb_sig *sig,
+		   const uint8_t *digest)
+{
+	/* Key and signature algorithms must match */
+	if (key->sig_alg != sig->sig_alg)
+		return BDB_ERROR_SIG_ALG;
+
+	switch (key->sig_alg) {
+	case BDB_SIG_ALG_RSA4096:
+		if (bdb_rsa4096_verify(key->key_data, sig->sig_data, digest))
+			return BDB_ERROR_VERIFY_SIG;
+		break;
+	case BDB_SIG_ALG_ECSDSA521:
+		if (bdb_ecdsa521_verify(key->key_data, sig->sig_data, digest))
+			return BDB_ERROR_VERIFY_SIG;
+		break;
+	case BDB_SIG_ALG_RSA3072B:
+		if (bdb_rsa3072b_verify(key->key_data, sig->sig_data, digest))
+			return BDB_ERROR_VERIFY_SIG;
+		break;
+	default:
+		return BDB_ERROR_VERIFY_SIG;
+	}
+
+	return BDB_SUCCESS;
+}
+
+int bdb_verify(const void *buf, size_t size, const uint8_t *bdb_key_digest)
+{
+	const uint8_t *end = (const uint8_t *)buf + size;
+	const struct bdb_header *h;
+	const struct bdb_key *bdbkey, *subkey;
+	const struct bdb_sig *sig;
+	const struct bdb_data *data;
+	const void *oem;
+	uint8_t digest[BDB_SHA256_DIGEST_SIZE];
+	int bdb_digest_mismatch;
+
+	/* Make sure buffer doesn't wrap around address space */
+	if (end < (const uint8_t *)buf)
+		return BDB_ERROR_BUF_SIZE;
+
+	/*
+	 * Check header now that we've actually loaded it.  We can't guarantee
+	 * this is the same header which was checked before.
+	 */
+	h = bdb_get_header(buf);
+	if (bdb_check_header(h, size))
+		return BDB_ERROR_HEADER;
+
+	/* Sanity-check BDB key */
+	bdbkey = bdb_get_bdbkey(buf);
+	if (bdb_check_key(bdbkey, end - (const uint8_t *)bdbkey))
+		return BDB_ERROR_BDBKEY;
+
+	/* Calculate BDB key digest and compare with expected */
+	if (bdb_sha256(digest, bdbkey, bdbkey->struct_size))
+		return BDB_ERROR_DIGEST;
+
+	bdb_digest_mismatch = memcmp(digest, bdb_key_digest, sizeof(digest));
+
+	/* Make sure OEM area 0 fits */
+	oem = bdb_get_oem_area_0(buf);
+	if (h->oem_area_0_size > end - (const uint8_t *)oem)
+		return BDB_ERROR_OEM_AREA_0;
+
+	/* Sanity-check subkey */
+	subkey = bdb_get_subkey(buf);
+	if (bdb_check_key(subkey, end - (const uint8_t *)subkey))
+		return BDB_ERROR_SUBKEY;
+
+	/* Make sure enough data was signed, and the signed data fits */
+	if (h->oem_area_0_size + subkey->struct_size > h->signed_size ||
+	    h->signed_size > end - (const uint8_t *)oem)
+		return BDB_ERROR_BDB_SIGNED_SIZE;
+
+	/* Sanity-check header signature */
+	sig = bdb_get_header_sig(buf);
+	if (bdb_check_sig(sig, end - (const uint8_t *)sig))
+		return BDB_ERROR_HEADER_SIG;
+
+	/* Make sure it signed the right amount of data */
+	if (sig->signed_size != h->signed_size)
+		return BDB_ERROR_HEADER_SIG;
+
+	/* Calculate header digest and compare with expected signature */
+	if (bdb_sha256(digest, oem, h->signed_size))
+		return BDB_ERROR_DIGEST;
+	if (bdb_verify_sig(bdbkey, sig, digest))
+		return BDB_ERROR_HEADER_SIG;
+
+	/*
+	 * Sanity-check data struct.  This also checks that OEM area 1 and the
+	 * hashes fit in the remaining buffer.
+	 */
+	data = bdb_get_data(buf);
+	if (bdb_check_data(data, end - (const uint8_t *)data))
+		return BDB_ERROR_DATA;
+
+	/* Sanity-check data signature */
+	sig = bdb_get_data_sig(buf);
+	if (bdb_check_sig(sig, end - (const uint8_t *)sig))
+		return BDB_ERROR_DATA_SIG;
+	if (sig->signed_size != data->signed_size)
+		return BDB_ERROR_DATA_SIG;
+
+	/* Calculate data digest and compare with expected signature */
+	if (bdb_sha256(digest, data, data->signed_size))
+		return BDB_ERROR_DIGEST;
+	if (bdb_verify_sig(subkey, sig, digest))
+		return BDB_ERROR_DATA_SIG;
+
+	/* Return success or success-other-than-BDB-key-mismatch */
+	return bdb_digest_mismatch ? BDB_GOOD_OTHER_THAN_KEY : BDB_SUCCESS;
+}
---
+++ bdb/host.c
@@ -0,0 +1,347 @@
+/* Copyright (c) 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Host functions for signing
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "bdb.h"
+#include "host.h"
+
+char *strzcpy(char *dest, const char *src, size_t size)
+{
+	strncpy(dest, src, size);
+	dest[size - 1] = 0;
+	return dest;
+}
+
+uint8_t *read_file(const char *filename, uint32_t *size_ptr)
+{
+	FILE *f;
+	uint8_t *buf;
+	long size;
+
+	*size_ptr = 0;
+
+	f = fopen(filename, "rb");
+	if (!f) {
+		fprintf(stderr, "Unable to open file %s\n", filename);
+		return NULL;
+	}
+
+	fseek(f, 0, SEEK_END);
+	size = ftell(f);
+	rewind(f);
+
+	if (size < 0 || size > UINT32_MAX) {
+		fclose(f);
+		return NULL;
+	}
+
+	buf = malloc(size);
+	if (!buf) {
+		fclose(f);
+		return NULL;
+	}
+
+	if (1 != fread(buf, size, 1, f)) {
+		fprintf(stderr, "Unable to read file %s\n", filename);
+		fclose(f);
+		free(buf);
+		return NULL;
+	}
+
+	fclose(f);
+
+	*size_ptr = size;
+	return buf;
+}
+
+int write_file(const char *filename, const void *buf, uint32_t size)
+{
+	FILE *f = fopen(filename, "wb");
+
+	if (!f) {
+		fprintf(stderr, "Unable to open file %s\n", filename);
+		return 1;
+	}
+
+	if (1 != fwrite(buf, size, 1, f)) {
+		fprintf(stderr, "Unable to write to file %s\n", filename);
+		fclose(f);
+		unlink(filename);  /* Delete any partial file */
+		return 1;
+	}
+
+	fclose(f);
+	return 0;
+}
+
+struct rsa_st *read_pem(const char *filename)
+{
+	struct rsa_st *pem;
+	FILE *f;
+
+	/* Read private key */
+	f = fopen(filename, "rb");
+	if (!f) {
+		fprintf(stderr, "%s: unable to read key from %s\n",
+			__func__, filename);
+		return NULL;
+	}
+
+	pem = PEM_read_RSAPrivateKey(f, NULL, NULL, NULL);
+	fclose(f);
+
+	return pem;
+}
+
+struct bdb_key *bdb_create_key(const char *filename,
+			       uint32_t key_version,
+			       const char *desc)
+{
+	uint32_t sig_alg;
+	size_t key_size = sizeof(struct bdb_key);
+	struct bdb_key *k;
+	uint8_t *kdata;
+	uint32_t kdata_size = 0;
+
+	/*
+	 * Read key data.  Somewhat lame assumption that we can determine the
+	 * signature algorithm from the key size, but it's true right now.
+	 */
+	kdata = read_file(filename, &kdata_size);
+	if (kdata_size == BDB_RSA4096_KEY_DATA_SIZE) {
+		sig_alg = BDB_SIG_ALG_RSA4096;
+	} else if (kdata_size == BDB_RSA3072B_KEY_DATA_SIZE) {
+		sig_alg = BDB_SIG_ALG_RSA3072B;
+	} else {
+		fprintf(stderr, "%s: bad key size from %s\n",
+			__func__, filename);
+		return NULL;
+	}
+	key_size += kdata_size;
+
+	/* Allocate buffer */
+	k = (struct bdb_key *)calloc(key_size, 1);
+	if (!k) {
+		free(kdata);
+		return NULL;
+	}
+
+	k->struct_magic = BDB_KEY_MAGIC;
+	k->struct_major_version = BDB_KEY_VERSION_MAJOR;
+	k->struct_minor_version = BDB_KEY_VERSION_MINOR;
+	k->struct_size = key_size;
+	k->hash_alg = BDB_HASH_ALG_SHA256;
+	k->sig_alg = sig_alg;
+	k->key_version = key_version;
+
+	/* Copy description, if any */
+	if (desc)
+		strzcpy(k->description, desc, sizeof(k->description));
+
+	/* Copy key data */
+	memcpy(k->key_data, kdata, kdata_size);
+	free(kdata);
+
+	return k;
+}
+
+struct bdb_sig *bdb_create_sig(const void *data,
+			       size_t size,
+			       struct rsa_st *key,
+			       uint32_t sig_alg,
+			       const char *desc)
+{
+	static const uint8_t info[] = {
+		0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86,
+		0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05,
+		0x00, 0x04, 0x20
+	};
+
+	size_t sig_size = sizeof(struct bdb_sig);
+	uint8_t digest[sizeof(info) + BDB_SHA256_DIGEST_SIZE];
+	struct bdb_sig *sig;
+
+	if (size >= UINT32_MAX)
+		return NULL;
+
+	switch(sig_alg) {
+	case BDB_SIG_ALG_RSA4096:
+		sig_size += BDB_RSA4096_SIG_SIZE;
+		break;
+	case BDB_SIG_ALG_RSA3072B:
+		sig_size += BDB_RSA3072B_SIG_SIZE;
+		break;
+	default:
+		fprintf(stderr, "%s: bad signature algorithm %d\n",
+			__func__, sig_alg);
+		return NULL;
+	}
+
+	/* Allocate buffer */
+	sig = (struct bdb_sig *)calloc(sig_size, 1);
+	if (!sig)
+		return NULL;
+
+	sig->struct_magic = BDB_SIG_MAGIC;
+	sig->struct_major_version = BDB_SIG_VERSION_MAJOR;
+	sig->struct_minor_version = BDB_SIG_VERSION_MINOR;
+	sig->struct_size = sig_size;
+	sig->hash_alg = BDB_HASH_ALG_SHA256;
+	sig->sig_alg = sig_alg;
+	sig->signed_size = size;
+
+	/* Copy description, if any */
+	if (desc)
+		strzcpy(sig->description, desc, sizeof(sig->description));
+
+	/* Calculate info-padded digest */
+	memcpy(digest, info, sizeof(info));
+	if (bdb_sha256(digest + sizeof(info), data, size)) {
+		free(sig);
+		return NULL;
+	}
+
+	/* RSA-encrypt the signature */
+	if (RSA_private_encrypt(sizeof(digest),
+				digest,
+				sig->sig_data,
+				key,
+				RSA_PKCS1_PADDING) == -1) {
+		free(sig);
+		return NULL;
+	}
+	return sig;
+}
+
+struct bdb_header *bdb_create(struct bdb_create_params *p)
+{
+	size_t bdb_size = 0;
+	size_t sig_size = sizeof(struct bdb_sig) + BDB_RSA4096_SIG_SIZE;
+	size_t hashes_size = sizeof(struct bdb_hash) * p->num_hashes;
+	uint8_t *buf, *bnext;
+	struct bdb_header *h;
+	struct bdb_sig *sig;
+	struct bdb_data *data;
+	const void *oem;
+
+	/* We can do some checks before we even allocate the buffer */
+
+	/* Make sure OEM sizes are aligned */
+	if ((p->oem_area_0_size & 3) || (p->oem_area_1_size & 3)) {
+		fprintf(stderr, "%s: OEM areas not 32-bit aligned\n",
+			__func__);
+		return NULL;
+	}
+
+	/* Hash count must fit in uint8_t */
+	if (p->num_hashes > 255) {
+		fprintf(stderr, "%s: too many hashes\n", __func__);
+		return NULL;
+	}
+
+	/* Calculate BDB size */
+	bdb_size = sizeof(struct bdb_header);
+	bdb_size += p->bdbkey->struct_size;
+	bdb_size += p->oem_area_0_size;
+	bdb_size += p->subkey->struct_size;
+	bdb_size += sig_size;
+	bdb_size += sizeof(struct bdb_data);
+	bdb_size += p->oem_area_1_size;
+	bdb_size += sizeof(struct bdb_hash) * p->num_hashes;
+	bdb_size += sig_size;
+
+	/* Make sure it fits */
+	if (bdb_size > UINT32_MAX) {
+		fprintf(stderr, "%s: BDB size > UINT32_MAX\n", __func__);
+		return NULL;
+	}
+
+	/* Allocate a buffer */
+	bnext = buf = calloc(bdb_size, 1);
+	if (!buf) {
+		fprintf(stderr, "%s: can't allocate buffer\n", __func__);
+		return NULL;
+	}
+
+	/* Fill in the header */
+	h = (struct bdb_header *)bnext;
+	h->struct_magic = BDB_HEADER_MAGIC;
+	h->struct_major_version = BDB_HEADER_VERSION_MAJOR;
+	h->struct_minor_version = BDB_HEADER_VERSION_MINOR;
+	h->struct_size = sizeof(*h);
+	h->bdb_load_address = p->bdb_load_address;
+	h->bdb_size = bdb_size;
+	h->signed_size = p->oem_area_0_size + p->subkey->struct_size;
+	h->oem_area_0_size = p->oem_area_0_size;
+	bnext += h->struct_size;
+
+	/* Copy BDB key */
+	memcpy(bnext, p->bdbkey, p->bdbkey->struct_size);
+	bnext += p->bdbkey->struct_size;
+
+	/* Copy OEM area 0 */
+	oem = bnext;
+	if (p->oem_area_0_size) {
+		memcpy(bnext, p->oem_area_0, p->oem_area_0_size);
+		bnext += p->oem_area_0_size;
+	}
+
+	/* Copy subkey */
+	memcpy(bnext, p->subkey, p->subkey->struct_size);
+	bnext += p->subkey->struct_size;
+
+	/*
+	 * Create header signature using private BDB key.
+	 *
+	 * TODO: create the header signature in a totally separate step.  That
+	 * way, the private BDB key is not required each time a BDB is created.
+	 */
+	sig = bdb_create_sig(oem, h->signed_size, p->private_bdbkey,
+			     p->bdbkey->sig_alg, p->header_sig_description);
+	memcpy(bnext, sig, sig->struct_size);
+	bnext += sig->struct_size;
+
+	/* Fill in the data */
+	data = (struct bdb_data *)bnext;
+	data->struct_magic = BDB_DATA_MAGIC;
+	data->struct_major_version = BDB_DATA_VERSION_MAJOR;
+	data->struct_minor_version = BDB_DATA_VERSION_MINOR;
+	data->struct_size = sizeof(struct bdb_data);
+	data->data_version = p->data_version;
+	data->oem_area_1_size = p->oem_area_1_size;
+	data->num_hashes = p->num_hashes;
+	data->hash_entry_size = sizeof(struct bdb_hash);
+	data->signed_size = data->struct_size + data->oem_area_1_size +
+		hashes_size;
+	if (p->data_description) {
+		strzcpy(data->description, p->data_description,
+			sizeof(data->description));
+	}
+	bnext += data->struct_size;
+
+	/* Copy OEM area 1 */
+	oem = bnext;
+	if (p->oem_area_1_size) {
+		memcpy(bnext, p->oem_area_1, p->oem_area_1_size);
+		bnext += p->oem_area_1_size;
+	}
+
+	/* Copy hashes */
+	memcpy(bnext, p->hash, hashes_size);
+	bnext += hashes_size;
+
+	/* Create data signature using private subkey */
+	sig = bdb_create_sig(data, data->signed_size, p->private_subkey,
+			     p->subkey->sig_alg, p->data_sig_description);
+	memcpy(bnext, sig, sig->struct_size);
+
+	/* Return the BDB */
+	return h;
+}
