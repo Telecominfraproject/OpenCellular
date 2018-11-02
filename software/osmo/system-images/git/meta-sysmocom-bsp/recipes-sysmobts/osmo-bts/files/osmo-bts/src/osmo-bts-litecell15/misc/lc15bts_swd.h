b3744e3ee51176015051c070c1e5e10757762d2f
Omar Ramadan <oramadan@fb.com>
2018-11-02 12:00:43 -0700

    Use osmo-bts upstream
    Change-Id: Ifca0e9b60874f5752551ea8c928141ff2beacf11

--- software/osmo/system-images/git/meta-sysmocom-bsp/recipes-sysmobts/osmo-bts/files/osmo-bts/src/osmo-bts-litecell15/misc/lc15bts_swd.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LC15BTS_SWD_H
-#define _LC15BTS_SWD_H
-
-int lc15bts_swd_init(struct lc15bts_mgr_instance *mgr, int swd_num_events);
-int lc15bts_swd_event(struct lc15bts_mgr_instance *mgr, enum mgr_swd_events swd_event);
-
-#endif
