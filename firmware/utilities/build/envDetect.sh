4dac1a2744dfb6d5c760af574f4b2683a45cfb4c
jspectacular <marriott@fb.com>
2018-11-06 23:54:43 +0000

    Issue # 171

---
+++ firmware/utilities/build/envDetect.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -e
+BUILDHOST=$(uname -a)
+GCCVER=$(gcc --version)
+GITHASH=$(git rev-parse HEAD)
+if [ $# = 2 ]; then
+  BUILDTYPE="\tEC Build"
+  BUILDFNAME="build_ec_cfg.txt"
+  if [ -e "$1" ]; then
+    ARMVER=$($1 --version)
+  else
+    ARMVER="arm-none-eabi-gcc: Not found"
+  fi
+  if [ -d "$2" ]; then
+    TIRTOSVER="TI RTOS: $2"
+  else
+    TIRTOSVER="TI RTOS: Not found"
+  fi
+else
+  BUILDTYPE="\tHost Build"
+  BUILDFNAME="build_host_cfg.txt"
+  ARMVER=""
+  TIRTOSVER=""
+fi
+{
+echo "$BUILDTYPE"
+echo
+echo "GIT Hash: $GITHASH"
+echo
+echo "Build Host: $BUILDHOST"
+echo
+echo "$GCCVER"
+echo
+echo "$ARMVER"
+echo
+echo "$TIRTOSVER"
+} > $BUILDFNAME
