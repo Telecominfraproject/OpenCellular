c7721f934ae3dc5ba1e5b698eede5fba1c631899
jspectacular <marriott@fb.com>
2018-11-08 03:39:56 +0000

    Environment detection from unit test

--- firmware/utilities/build/envDetect.sh
+++ firmware/utilities/build/envDetect.sh
@@ -5,6 +5,7 @@ GITHASH=$(git rev-parse HEAD)
 if [ $# = 2 ]; then
   BUILDTYPE="\tEC Build"
   BUILDFNAME="build_ec_cfg.txt"
+  UNITYVER=""
   if [ -e "$1" ]; then
     ARMVER=$($1 --version)
   else
@@ -15,22 +16,38 @@ if [ $# = 2 ]; then
   else
     TIRTOSVER="TI RTOS: Not found"
   fi
+elif [ $# = 1 ]; then
+  BUILDTYPE="\tUnit Test Build"
+  BUILDFNAME="build_ut_cfg.txt"
+  ARMVER=""
+  TIRTOSVER=""
+  UNITYVER="Unity: $1"
 else
   BUILDTYPE="\tHost Build"
   BUILDFNAME="build_host_cfg.txt"
   ARMVER=""
   TIRTOSVER=""
+  UNITYVER=""
 fi
 {
 echo "$BUILDTYPE"
 echo
+if [ ! "$UNITYVER" = "" ]; then
+  echo "$UNITYVER"
+  echo
+fi
 echo "Git Hash: $GITHASH"
 echo
 echo "Build Host: $BUILDHOST"
 echo
+if [ ! "$TIRTOSVER" = "" ]; then
+  echo "$TIRTOSVER"
+  echo
+fi
 echo "GCC: $GCCVER"
 echo
-echo "$ARMVER"
-echo
-echo "$TIRTOSVER"
+if [ ! "$ARMVER" = "" ]; then
+  echo "$ARMVER"
+  echo
+fi
 } > $BUILDFNAME
