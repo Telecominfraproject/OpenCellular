7937cbca777726efe88b87d3393cf7a35e09718f
Jack Marriott <marriott@fb.com>
2018-11-23 05:00:29 -0500

    Add hash for EC binaries

--- firmware/utilities/checksums/hosthash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-#
-# Description
-#   This script records the md5 hashes of host build objects
-#
-BIN_FILES=$(find $1 -executable -type f)
-HOST_MD5_FILE="hmd5sums.txt"
-md5sum $BIN_FILES > $HOST_MD5_FILE
