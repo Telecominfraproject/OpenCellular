99449927d82e608cf3a479ba8361709d3f7910fc
Jack Marriott <marriott@fb.com>
2018-11-28 20:13:21 -0500

    Fixing gcc build warnings

--- firmware/ec/inc/devices/at45db.h
+++ firmware/ec/inc/devices/at45db.h
@@ -45,7 +45,7 @@ typedef struct AT45DB_Dev {
 ePostCode at45db_probe(AT45DB_Dev *dev, POSTData *postData);
 ReturnStatus at45db_data_read(AT45DB_Dev *dev, uint8_t *data,
                               uint32_t data_size, uint32_t byte, uint32_t page);
-ReturnStatus at45db_data_write(AT45DB_Dev *dev, uint8_t *data,
+ReturnStatus at45db_data_write(AT45DB_Dev *dev, const uint8_t *data,
                                uint32_t data_size, uint32_t byte,
                                uint32_t page);
 ReturnStatus at45db_erasePage(AT45DB_Dev *dev, uint32_t page);
