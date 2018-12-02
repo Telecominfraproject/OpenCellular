6218e6b1930c51083c56d3141f0f12494b3f2041
Jack Marriott <39570581+jspectacular@users.noreply.github.com>
2018-12-02 01:14:31 -0800

    Update g510.h

--- firmware/ec/inc/common/g510.h
+++ firmware/ec/inc/common/g510.h
@@ -24,14 +24,15 @@
 /*****************************************************************************
  *                             FUNCTION DECLARATIONS
  *****************************************************************************/
-ePostCode g510_task_init(void *driver, const void *config,
-                         const void *alert_token);
+bool g510_get_ber(TestMod_2G_Status_Data *p2gStatusData);
+bool g510_get_cellId(TestMod_2G_Status_Data *p2gStatusData);
 bool g510_get_imei(TestMod_2G_Status_Data *p2gStatusData);
 bool g510_get_imsi(TestMod_2G_Status_Data *p2gStatusData);
 bool g510_get_mfg(TestMod_2G_Status_Data *p2gStatusData);
 bool g510_get_model(TestMod_2G_Status_Data *p2gStatusData);
-bool g510_get_rssi(TestMod_2G_Status_Data *p2gStatusData);
-bool g510_get_ber(TestMod_2G_Status_Data *p2gStatusData);
 bool g510_get_regStatus(TestMod_2G_Status_Data *p2gStatusData);
-bool g510_get_cellId(TestMod_2G_Status_Data *p2gStatusData);
+bool g510_get_rssi(TestMod_2G_Status_Data *p2gStatusData);
+ePostCode g510_task_init(void *driver, const void *config,
+                         const void *alert_token);
+
 #endif /* G510_H_ */
