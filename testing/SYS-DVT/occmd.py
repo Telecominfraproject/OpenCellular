ee6c14c195e11129ada5e71776ea2a060b777f8d
swateeshrivastava <swatee.shrivastava@aricent.com>
2019-01-11 18:04:33 +0530

    parsing update

--- testing/SYS-DVT/occmd.py
+++ testing/SYS-DVT/occmd.py
@@ -4,9 +4,41 @@ import json
 
 DB_FILE = "data.json"
 db = '';
+module = ''
+operation = ''
+param = ''
+value = ''
 
-def parse_it(args):
-    for 
+def check_args(args):
+    global module
+    global operation
+    global param
+    global value
+    
+    # check for module name
+    if((len(args) < 2) or (args[1] == 'help')):
+        return -1
+
+    for x in db['module']:
+        if(x == args[1]):
+            module = x
+            # Check for get/set
+            if(len(args) < 3 and (args[2] == 'get' or args[2] == 'set')):
+                return -1
+            else:
+                operation = args[2]
+                if(operation == 'set'):
+                    if(len(args) < 5):
+                        return -1
+                    else:
+                        param = args[3]
+                        value = args[4]
+                else: # get
+                    if(len(args) < 4):
+                        return -1
+                    else:
+                        param = args[3]
+   
     return 0
 
 def load_database():
@@ -17,8 +49,13 @@ def load_database():
 
 def main():
     load_database()
-    print db
-    parse_it(sys.argv)
+    if(check_args(sys.argv)):
+         print db['module']['help']
+         sys.exit(-1)
+
+   # We have a valid request request process it
+    print "Request valid" 
+
     sys.exit(0)
 
 
